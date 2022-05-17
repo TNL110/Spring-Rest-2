02692310a6f5d3fea47e951366ed1b7048829d07
TNL110 <phuclocloi123@gmail.com>
2022-05-17 17:51:26 +0700

    the first commit

---
+++ src/main/java/tacos/TacoCloud7Application.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloud7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloud7Application.class, args);
+	}
+
+}
